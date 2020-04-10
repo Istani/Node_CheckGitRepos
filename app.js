378f87fd6841b91bb2794869d30b4605ea6794ff
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-04-10 14:41:16 +0200

    'Backup Sync'

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ async function get_repos() {
 	process.chdir(element.name);
 	await child_process.spawnSync("git", ["checkout", "master"]);
 	await child_process.spawnSync("git", ["pull", "--all"]);
+	await child_process.spawnSync("git", ["add", "."]);
+	await child_process.spawnSync("git", ["commit", "-m", "'Backup Sync'"]);
+	await child_process.spawnSync("git", ["push", "--all"]);
 	//await child_process.spawnSync("git", ["submodule", "init"]);
 	//await child_process.spawnSync("git", ["submodule", "update"]);
 	process.chdir("../");
